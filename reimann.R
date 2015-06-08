604566fec84f7d3fd268471f4a977f9e7371a8ac
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-06-08 11:48:33 -0700

    Update reimann.R

--- reimann.R
+++ reimann.R
@@ -8,15 +8,31 @@ integrate(integrand, 0, Inf)
 
 #e[x]
 #simple monte carlo
-m = 10^7; a = 0; b = 20
+m = 1000000; a = 0; b = 20
 w = (b - a)/m # average distance btw random pts
 u = runif(m, a, b) # random points
 h = u * dexp(u,rate=3) # heights above random points
 sum(w*h) 
 
 #reiman to approximate e[x]
-m = 100000; a = 0; b = 20 # Try 20 (and more grid pts) to check
+m = 100000; a = 0; b = 20 
 w = (b - a)/m # width (base) of rectangles
 g = seq(a+w/2, b, by=w) # grid of m centers
 h = g * dexp(g,rate=3) # heights of rectangles
 sum(w*h) 
+
+
+
+#finding p(.5<X<1.5) simple monte carlo
+m = 100000; a = 0.5; b = 1.5
+w = (b - a)/m # average distance btw random pts
+u = runif(m, a, b) # random points
+h = dexp(u,rate=3) # heights above random points
+sum(w*h) 
+
+#acceptance rejection for p(.5<X<1.5)
+m = 10^6; a = 0.5; b = 1.5
+u = runif(m, a, b); h = runif(m, 0, 0.8) # random pts in rectangle
+frac.acc = mean(h < dexp(u,rate=3))
+area = (b-a)*0.8
+area*frac.acc 
