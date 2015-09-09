6299668a089a0f5e291914b8d39c017b13fc9f3b
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-09-08 21:01:27 -0700

    Create sqldf.R
    Example of using the sqldf library.

---
+++ sqldf.R
@@ -0,0 +1,22 @@
+scores <- data.frame (name2 =c("Kim","Bob","Ted","Sue","Liz","Amanada","Tricia","Johnathan","Luis","Isabel"),
+ name =c(17,19,24,25,16,15,23,24,29,17) )
+people
+scores
+
+#creates data frame
+quiz_scores =  data.frame(people, scores)
+quiz_scores
+#[] gives the column for the row
+quiz_scores[2]
+#gives the same thing
+quiz_scores$scores
+
+#creates a spreadsheet to put data in
+Min_Wage = data.frame(Year=numeric(), Value=numeric())
+Min_Wage =  edit(Min_Wage)
+Min_Wage
+
+install.packages("sqldf");
+library(sqldf);
+
+temp <- sqldf("SELECT * from scores s inner join SELECT *  Min_Wage on scores = Min_Wage.Year");
