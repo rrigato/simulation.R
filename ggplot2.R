be2f9d2d795733b035d9fe812744e0bece4a0a4e
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-09-10 21:17:12 -0700

    Update ggplot2.R

--- ggplot2.R
+++ ggplot2.R
@@ -8,6 +8,13 @@ head(WR_data)
 
 qplot(weight, aDOT, data = WR_data, color = Height);
 
-qplot(Height, aDOT, data = WR_data, size = weight, color = age,
+q <- qplot(Height, aDOT, data = WR_data, size = weight, color = age,
 	xlab = "Wide Reciever Height", ylab = " Wide Reciever aDOT", 
 	main = "Comparing WR height and aDOT") 
+
+#can do geom = bar or geom = line for different plots
+#geom = c("point", "line")
+
+
+z <- qplot(Height, aDOT, data = WR_data, color = )
+z  + scale_color_brewer(palette="Set2")
