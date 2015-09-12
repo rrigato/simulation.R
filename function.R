ba3d924dec14eeb52ad842721f14b3ab4dc64340
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-09-12 12:08:38 -0700

    Create function.R
    Using R for classification and building a function for regression in R.

---
+++ function.R
@@ -0,0 +1,45 @@
+install.packages("ISLR")
+library(ISLR)
+library(MASS)
+attach (Boston)
+
+lm_model = lm(medv~lstat)
+lm_model
+summary(lm_model)
+
+names(lm_model)
+lm_model$residuals
+confint(lm_model)
+
+library(ggplot2)
+
+ test = qplot(lstat,medv, data=lm_model)
+test2 = plot(lstat,medv)
+abline(lm_model)
+test
+par(mfrow=c(2,2))
+test2
+install.packages("lubridate")
+qplot(residuals(lm_model), predict(lm_model), data = lm_model
+ color = green)
+
+lm_model2 = lm(lstat~medv + medv:age)
+summary(lm_model2)
+
+LoadLibraries= function() {
+	library(ggplot2);
+	library(lubridate);
+	print("The libraries have been loaded");
+}
+LoadLibraries
+LoadLibraries()
+
+Regression = function( Y, X1, dataset){
+	temp = lm(Y~ X1 , data=dataset); print(temp);
+	print(summary(temp))
+	plot(temp$residuals, temp$predict)
+}
+
+head(Boston)
+par(mfrow=c(1,1))
+Regression(lstat, medv, Boston)
