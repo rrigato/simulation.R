cc0126cba3b8b9da2bf384f5b10f13ce18b3a5ea
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-09-12 22:09:39 -0700

    Update statistical_learning.R
    Added linear descreant, quadratic descreant models and k nearest neighbors (knn) models

--- statistical_learning.R
+++ statistical_learning.R
@@ -64,3 +64,80 @@ probs = predict(log_model, type = "response")
 #contrast tells which way the qualitative is coded in R
 # prob[1] means their is a .507 chance of direction = up
 probs[1:10]; contrasts(Direction)
+
+
+
+
+
+
+library(MASS); library(ISLR)
+head(Smarket)
+lda_model = lda(Direction~ Lag1 + Lag2, data = Smarket, subset = train)
+lda_model; plot(lda_model)
+lda_predict = predict(lda_model, Smarket.2005)
+names(lda_predict)
+lda_class = lda_predict$class
+table(lda_class, Direction.2005)
+mean(lda_class == Direction.2005)
+
+#using a different probability threshold for the posterior
+sum(lda_predict$posterior[,1] >=.9)
+
+
+
+#qda model(drops assumption of constant variance among classes
+qda.fit = qda(Direction~ Lag1 + Lag2, data = Smarket, subset =train)
+qda.fit
+qda.class = predict(qda.fit, Smarket.2005)$class
+table(qda.class, Direction.2005)
+mean(qda.class == Direction.2005)
+
+
+
+#k nearest neighbors model
+library(class)
+attach(Smarket)
+train.X = cbind(Lag1,Lag2)[train,]
+test.X = cbind(Lag1,Lag2)[!train,]
+train.Direction = Direction[train]
+
+set.seed(1)
+knn.pred = knn(train.X,test.X, train.Direction, k=1)
+table(knn.pred, Direction.2005)
+mean(knn.pred == Direction.2005) #.500 == not good
+
+knn.pred = knn(train.X,test.X, train.Direction, k=3)
+table(knn.pred, Direction.2005)
+mean(knn.pred == Direction.2005) #.527 for k = 3
+
+
+attach(Caravan)
+#standardizes the values to mean 0 and sd of 1
+Standardized.X = scale(Caravan[,-86])
+var(Caravan[,1]); var (Caravan[,2]); 
+var(Standardized.X[,1]); var(Standardized.X[,2])
+test = 1:1000
+train.X = Standardized.X[-test,] #indicies that do not range from 1:1000
+test.X = Standardized.X[test,]
+train.Y = Purchase[-test] #indicies that do not range from 1:1000
+test.Y = Purchase[test]
+set.seed(1)
+knn.pred2 = knn(train.X, test.X, train.Y, k=1)
+mean(test.Y!=knn.pred2)  #.118 error rate
+mean(test.Y != "No")  #.059
+
+knn.pred3 = knn(train.X, test.X, train.Y, k=3)
+mean(test.Y !=knn.pred3)
+table(knn.pred3,test.Y) #5/26 when actual is .06
+
+knn.pred4 = knn(train.X, test.X, train.Y, k=5)
+mean(test.Y !=knn.pred4)
+table(knn.pred4,test.Y) #4/15 when actual is .06
+
+
+
+
+
+
+
+
