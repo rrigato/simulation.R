282acf0b2df93f301d6d5b72608e646df4fb55a2
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-10-07 16:24:46 -0700

    Update mathstat.R

--- mathstat.R
+++ mathstat.R
@@ -1,3 +1,5 @@
+
+#problem 58
 set.seed(2842)
 Pop = 1:100; sample_distribution = numeric(100);
 for(i in 1:100)
@@ -6,3 +8,17 @@ for(i in 1:100)
 }
 
 hist(sample_distribution)
+
+
+
+
+
+
+#problem 67 part a)
+families = read.csv(url("http://statistics.csueastbay.edu/~jkerr/STAT65012/families.txt"), header=T)
+families[1:10,]
+
+set.seed(2842)
+for (z in 1:5)
+{
+}
