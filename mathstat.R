0f36446ecb88cd1df2bda4a953e69d3fd038eaaf
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-10-09 11:03:07 -0700

    Update mathstat.R

--- mathstat.R
+++ mathstat.R
@@ -18,10 +18,7 @@ hist(sample_distribution)
 families = read.csv(url("http://statistics.csueastbay.edu/~jkerr/STAT65012/families.txt"), header=T)
 families[1:10,]
 #43886
-library(ggplot2)
 
-summary(sample_distribution)
-dim(sample_distribution)
 set.seed(2842)
 size = 1:43886
 n = 500
@@ -36,16 +33,23 @@ for (z in 1:5) #repeats sampling process 5 times
 
 	#part i
 	#estimate of female headed families
-	p_hat = (sum(random_obs[1:n,1] == 3)/n); 
+	p_hat = (sum(random_obs[1:n,1] == 3)/n); p_hat
 	
 	#estimate of the standard error
-	porp_se = sqrt( ( (p_hat-(1-p_hat)/(n-1) ) * ((N-n)/N)) )
+	porp_se = sqrt( ( ( (p_hat*(1-p_hat))/(n-1) ) * ((N-n)/N)) )
 	porp_se
+	
+	#wife_lead_CI = p_hat +- 1.96*porp_se
+	
+	#part ii
+	random_num2 = sample(size,n) #gets random obs from 1 to 43886
+	random_obs2 = numeric(n); #initializes vector that will contain family values
+	#applies the observations that were randomly drawn
+	random_obs2 = families[random_num[1:n],];
+	children_se = ( sd(random_obs2[1:n,3]) / sqrt(n) ) * sqrt( ( (N-n) / N ) ); children_se
+	
+	#average children CI 
+	#ave_child_CI = mean(random_obs2[1:n,3]) +- 1.96*children_se
+
+
 }
-
-
-
-
-
-
-
