ab0981e58c9a9b8a88efa246325aaedf1de0e378
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-10-21 17:14:56 -0700

    Update mathstat2.R

--- mathstat2.R
+++ mathstat2.R
@@ -16,6 +16,12 @@ hist(likelihood, prob = TRUE)
 likelihood
 
 
+#vectorized solution
+n = 100:1000
+choose(n,5)
 
 
+results = numeric(1000-100)
+results = (choose(50,20) * choose(n - 100,30) ) / choose(n,50)
+results[1:100]
 
