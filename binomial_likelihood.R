84dcef89599b42bf1a1a4abec94d78da3b73de9a
Ryan Rigato <rrigato@horizon.csueastbay.edu>
2015-11-09 15:08:15 -0800

    Update binomial_likelihood.R

--- binomial_likelihood.R
+++ binomial_likelihood.R
@@ -30,3 +30,35 @@ for (i in 1:B)
 	n4 = Ran_value * .25 * MLE
 	result[i] = (.25 * n1 + n2 -n3 + n4)/ 3.25*n1 - 2 
 }
+
+
+
+
+
+
+
+#problem 58 d-e
+
+B = 15000
+
+result = numeric(B)
+
+MLE = .767
+
+n = 190
+Ran_value = n1 = n2 = n3 = n4 = numeric(1)
+
+for (i in 1:B)
+{
+	Ran_value = sample(n,1)
+	n1 = (Ran_value*(1-MLE)* (1-MLE))/n
+	n2 = (Ran_value*2*MLE*(1-MLE))/ n
+	n3 = (Ran_value * MLE*MLE)/n
+ 
+	result[i] = (n2 +2*n3)/ -1 * n1  + 1
+}
+
+sd(result)
+low_ci = quantile(result,.025); low_ci
+high_ci = quantile(result,.975); high_ci
+
